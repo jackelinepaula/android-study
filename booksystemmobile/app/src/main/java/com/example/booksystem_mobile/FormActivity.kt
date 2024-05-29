6fb602868eb6b35fc184528e5bc515cbd6b6331e
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2024-05-29 10:45:02 -0300

    FormActivity | POST implementado com okhttp3

--- booksystemmobile/app/src/main/java/com/example/booksystem_mobile/FormActivity.kt
+++ booksystemmobile/app/src/main/java/com/example/booksystem_mobile/FormActivity.kt
@@ -1,17 +1,62 @@
 package com.example.booksystem_mobile
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import java.io.IOException
 
 class FormActivity : AppCompatActivity() {
+
+    val okhttp = OkHttpClient();
+    val gson = Gson();
 
     override fun onCreate(bundle: Bundle?) {
         super.onCreate(bundle)
         setContentView(R.layout.form_layout);
 
         val edtTitulo = findViewById<EditText>(R.id.edtTitulo)
-        val edtAutor = findViewById<EditText>(R.id.edtTitulo)
+        val edtAutor = findViewById<EditText>(R.id.edtAutor)
+
+        val btnSalvar = findViewById<Button>(R.id.btnSalvar)
+
+        btnSalvar.setOnClickListener{
+            val livro = Livro(1, edtTitulo.text.toString(), edtAutor.text.toString())
+            val livroJson = gson.toJson(livro)
+
+            val body = livroJson.toRequestBody("application/json".toMediaTypeOrNull())
+
+            val req = Request.Builder()
+                .post(body)
+                .url("https://android-jaaj-default-rtdb.firebaseio.com/livros.json")
+                .build()
+
+            val res = object : Callback{
+
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.e("reqlivro", "call ${call} | Exception ${e.message.toString()}" )
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val b = response?.body.toString()
+                    Log.i("reslivro", "Call ${call} | Resposta ${response.body.toString()}");
+
+                }
+
+            }
+
+            okhttp.newCall(req).enqueue(res)
+        }
+
 
         
 
