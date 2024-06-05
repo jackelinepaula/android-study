d49abdb3ac2968e7171be13dde876cd5ef106711
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2024-06-05 10:54:07 -0300

    Catálogo | Adicionando fetch dos dados no Firebase

--- booksystemmobile/app/src/main/java/com/example/booksystem_mobile/CatalogoActivity.kt
+++ booksystemmobile/app/src/main/java/com/example/booksystem_mobile/CatalogoActivity.kt
@@ -1,13 +1,17 @@
 package com.example.booksystem_mobile
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.RequestBody
 import okhttp3.Response
 import java.io.IOException
 
@@ -15,9 +19,21 @@ class CatalogoActivity : AppCompatActivity(){
     val okhttp = OkHttpClient()
     val gson = Gson()
 
+    val dados = ArrayList<Livro>()
+
     override fun onCreate(bundle: Bundle?) {
         super.onCreate(bundle)
         setContentView(R.layout.catagolo_layout);
+
+        val btnExcluir = findViewById<Button>(R.id.btnExcluir)
+
+        val recycle = findViewById<RecyclerView>(R.id.rcvLivros)
+
+        val adapter = RecyclerViewAdapter(dados)
+
+        recycle.adapter = adapter
+
+        recycle.layoutManager = LinearLayoutManager(this)
 
         val req = Request.Builder()
             .get()
@@ -27,16 +43,45 @@ class CatalogoActivity : AppCompatActivity(){
         val res = object : Callback{
 
             override fun onFailure(call: Call, e: IOException) {
-                TODO("Not yet implemented")
+                Log.i("failcatalogo", "call $call | Exception: ${e.localizedMessage}")
             }
 
             override fun onResponse(call: Call, response: Response) {
-                TODO("Not yet implemented")
+                val resposta = response.body
+                Log.i("rescatalogo", "call $call | \n Resposta: ${resposta}")
+
+                //Pegando o tipo HashMap para o Gson converter o JSON
+                val type = object : TypeToken<HashMap<String?, Livro?>?>(){}.type
+                //Fazendo a conversão
+                val mapConvertido : HashMap<String, Livro> = gson.fromJson(resposta?.string(), type)
+
+                val lista = ArrayList<Livro>()
+
+                mapConvertido.keys.forEach{
+                    val livro = mapConvertido[it]
+
+                    if(livro != null){
+                        livro.id = it
+                        lista.add(livro)
+                    }
+
+                }
+
+                this@CatalogoActivity.runOnUiThread(
+                    java.lang.Runnable {
+                        dados.clear()
+                        Log.i("rescatalogo", mapConvertido.toString())
+                        dados.addAll(lista)
+
+                        recycle.adapter?.notifyDataSetChanged()
+                    }
+                )
             }
 
         }
 
-
-
+        okhttp.newCall(req).enqueue(res)
     }
+
+
 }
